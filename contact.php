4a442e6c60e343a7d62ddc786803c7a2f1e7ab54
Craig Robinson <ehb14190@ds.strath.ac.uk>
2016-11-18 14:54:31 +0000

    twitter

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>About</title>
+        <title>Contact</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
@@ -81,15 +81,9 @@ form{
   <li><a href="register.php">Register</a></li>
 </ul>
         <div>
-            <p>Strathclyde University Concert Band is a large band, open to all woodwind and brass, percussion or string bass players of all abilities.</p>
-
-            <p>It prides itself in being completely open - so no auditions!  And if you would like to come along but don't have your own instrument, get in touch as the Uni has a number of instruments available for students to borrow: bass clarinet, baritone saxophone, bassoon, tenor horn, French horn, euphonium, tuba and percussion.</p>
-
-            <p>Each year, the band pulls off some great performances at venues such as the Barony Hall and the Winter Gardens of the People's Palace (Glasgow Green).</p>
-
-            <p>However, the other side of the band is its unique, vibrant and colourful post-rehearsal/concert scene, involving a pub, to which everyone is wholeheartedly encouraged to attend!</p>
-
-            <p>We meet on Thursday evenings, 5:30-7:30pm, with post-rehearsal socials afterwards in Bar Home on Albion Street.</p>
+            <p>Committee email: committee@sucb.org.uk</p>
+            <p>President Email: president@sucb.org.uk</p>
+        
         </div>
     </body>
 </html>
