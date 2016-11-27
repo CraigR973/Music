bd8c1ff0595022a03060e27f7506754daefd0aee
Cameron McCulloch <cameron.mcculloch.2014@uni.strath.ac.uk>
2016-11-27 19:49:21 +0000

    Update header, now includes link to music page

--- index.php
+++ index.php
@@ -24,15 +24,20 @@
             <li><a style="padding-top: 23px;" href="news.php">News</a></li>
             <li><a style="padding-top: 23px;" href="contact.php">Contact</a></li>
             <li><a style="padding-top: 23px;" href="about.php">About</a></li>
-            
-            
-            
+            <?php
+                if ($user !== "") { // Buttons that only show when logged in
+                    echo /** @lang HTML */
+                    '<li><a style="padding-top: 23px;" href="music.php">Music</a></li>';
+                }
+            ?>
+
+
             <li><a style="padding-top: 23px;">
-                <form method="post" href="https://www.google.co.uk/" id="searchsite">
-                    <input type="text" name="search" title="search" style="margin-left: 100px;">
-                    <input type="submit" name="submit" value="Search" href="https://www.google.co.uk/">
-                </form>
-                <!-- <button onclick="searchFunction()">Search</button> -->
+                    <form method="post" href="https://www.google.co.uk/" id="searchsite">
+                        <input type="text" name="search" title="search" style="margin-left: 100px;">
+                        <input type="submit" name="submit" value="Search" href="https://www.google.co.uk/">
+                    </form>
+                    <!-- <button onclick="searchFunction()">Search</button> -->
                 </a></li>
 
         </ul>
--- music.php
+++ music.php
@@ -22,29 +22,27 @@ and open the template in the editor.
     </head>
     <body>
         <ul>
-            <li><a class="active" href="index.php" style="font-size: 40px; background-color: black; ">SUCB</a></li>
+            <li><a class="active" href="index.php" style="font-size: 40px;">SUCB</a></li>
             <li><a style="padding-top: 23px;" href="news.php">News</a></li>
             <li><a style="padding-top: 23px;" href="contact.php">Contact</a></li>
             <li><a style="padding-top: 23px;" href="about.php">About</a></li>
-            <li><a style="padding-top: 23px;" href="music.php">Music</a></li>
-            <li>
-                <form method="post" action="search.php?go" id="searchsite">
-                    <input type="text" name="search" title="search" style="margin-left: 480px;">
-                    <input type="submit" name="submit" value="Search">
-                </form>
-                <!-- <button onclick="searchFunction()">Search</button> -->
-            </li>
+            <?php
+                if ($user !== "") { // Buttons that only show when logged in
+                    echo /** @lang HTML */
+                    '<li><a style="padding-top: 23px;" href="music.php">Music</a></li>';
+                }
+            ?>
+
+
+            <li><a style="padding-top: 23px;">
+                    <form method="post" href="https://www.google.co.uk/" id="searchsite">
+                        <input type="text" name="search" title="search" style="margin-left: 100px;">
+                        <input type="submit" name="submit" value="Search" href="https://www.google.co.uk/">
+                    </form>
+                    <!-- <button onclick="searchFunction()">Search</button> -->
+                </a></li>
 
         </ul>
-
-        <!--  <p id="demo2"></p>
-
-          <script>
-              function searchFunction() {
-                  var x = document.getElementById("mySearch").innerHTML;
-                  document.getElementById("demo2").innerHTML = x;
-              }
-              </script> -->
 
         <div style="background-color: white; float: left; width: 100%; margin-bottom: 0.5cm;">
 
@@ -60,10 +58,19 @@ and open the template in the editor.
                 document.getElementById("date").innerHTML = days[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()];
             </script>
 
-            <div style="float: right; padding-right: 20%;">
+            <div style="float: right; padding-right: 20%; padding-top: 30px; font-family: HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;">
                 <span style="float: right; align-items: flex-end; padding-bottom: 10px; width: 100%;">
-                    <a style="color: blue; text-decoration: none;" href="login.php">Sign in</a> with your SUCB ID or
-                    <a style="color: blue; text-decoration: none;" href="register.php">Register</a> to see band events
+                    <?php
+                        if ($user === "") {
+                            echo /** @lang HTML */
+                            "<a style='color: blue; text-decoration: none;' href='login.php'>Sign in</a> with your SUCB ID or
+                             <a style='color: blue; text-decoration: none;' href='register.php'>Register</a> to see band events";
+                        } else {
+                            echo /** @lang HTML */
+                            "Welcome back, $user";
+                        }
+                    ?>
+
                 </span>
             </div>
 
@@ -100,7 +107,7 @@ and open the template in the editor.
                     }
                 } else {
 
-                    echo $inst < 0 ?
+                    echo $inst <= 0 ?
                         '<p>There are no pieces available for your instrument yet.</p>' :
                         '<p>You need to be logged in to see this page</p>';
                 }
